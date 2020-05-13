336953ba3222ddb23541a29b40af2084f2bd3f44
charliejuc <charliejuc@gmail.com>
2020-05-13 11:28:27 +0200

    Test error path.
    modified:   tests/src/auth/infraestructure/repositories/auth-create-memory-repository.spec.ts

--- tests/src/auth/infraestructure/repositories/auth-create-memory-repository.spec.ts
+++ tests/src/auth/infraestructure/repositories/auth-create-memory-repository.spec.ts
@@ -2,31 +2,77 @@ import { Auth } from "@/auth/domain/auth";
 import { AuthObject } from "@/auth/domain/interfaces/auth-object";
 import { ErrorsObject } from "@/auth/domain/types/errors-object";
 import { AuthCreateMemoryRepository } from "@/auth/infraestructure/repositories/auth-create-memory-repository";
-import { AuthGeneratorFaker as AuthGenerator } from "../../domain/generator/auth-generator-faker";
+import { AuthMock } from "../../domain/auth-mock";
 
 const createMemoryRepository = new AuthCreateMemoryRepository();
-const authGenerator = new AuthGenerator();
+const authGenerator = AuthMock.AuthGenerator();
 
-test("should create auth entry successfully", async () => {
+describe("should create auth entry successfully", () => {
   const authObj = authGenerator.getInstance();
 
   let err: null | Error = null;
   let authInstance: Auth | ErrorsObject | null = null;
-  try {
-    authInstance = await createMemoryRepository.create(authObj);
-  } catch (e) {
-    err = e;
-  }
+  let authInstanceJSON: AuthObject;
+  beforeAll(async () => {
+    try {
+      authInstance = await createMemoryRepository.create(authObj);
+    } catch (e) {
+      err = e;
+    }
 
-  expect(authInstance).toBeTruthy();
-  expect(err).toBeFalsy();
-  expect(authInstance).toBeInstanceOf(Auth);
+    // @ts-ignore
+    authInstanceJSON = authInstance.toJSON();
+  });
 
-  // @ts-ignore
-  const authInstanceJSON: AuthObject = authInstance.toJSON();
+  test("should not throw an exception", () => {
+    expect(err).toBeFalsy();
+  });
 
-  expect(authInstanceJSON.id).toBe(authObj.id);
-  expect(authInstanceJSON.username).toBe(authObj.username);
-  expect(authInstanceJSON.email).toBe(authObj.email);
-  expect(authInstanceJSON.password).not.toBe(authObj.password);
+  test("should have valid Auth instance", () => {
+    expect(authInstance).toBeTruthy();
+    expect(authInstance).toBeInstanceOf(Auth);
+  });
+
+  test("should have correct JSON field values", () => {
+    expect(authInstanceJSON.id).toBe(authObj.id);
+    expect(authInstanceJSON.username).toBe(authObj.username);
+    expect(authInstanceJSON.email).toBe(authObj.email);
+    expect(authInstanceJSON.password).not.toBe(authObj.password);
+    expect(typeof authInstanceJSON.password).toBe("string");
+  });
+});
+
+describe("should fail creating auth entry", () => {
+  const authObj = {};
+
+  let err: null | Error = null;
+  let authInstance: Auth | ErrorsObject | null = null;
+  let errorsObj: ErrorsObject | Auth | null;
+  beforeAll(async () => {
+    try {
+      // @ts-ignore
+      authInstance = await createMemoryRepository.create(authObj);
+    } catch (e) {
+      err = e;
+    }
+
+    errorsObj = authInstance;
+
+    if (authInstance && authInstance.toJSON) {
+      // @ts-ignore
+      errorsObj = authInstance.toJSON();
+    }
+  });
+
+  test("should not throw an exception", () => {
+    expect(err).toBeFalsy();
+  });
+
+  test("should have errors", () => {
+    expect(errorsObj).toBeTruthy();
+    // @ts-ignore
+    expect(errorsObj.contructor).toBe({}.contructor);
+    // @ts-ignore
+    expect(Object.keys(errorsObj).length).toBeTruthy();
+  });
 });
